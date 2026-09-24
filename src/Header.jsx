70f05a67479bf2f3a955509bdd7d974067115904
Sohan68 <devsohan25@gmail.com>
2026-09-24 22:11:49 +0600

    This is my Task Manager Apps . There are Header , HeroSection & Footer .

---
+++ src/Header.jsx
@@ -0,0 +1,13 @@
+const Header = () => {
+  return (
+    <div>
+      <nav className="py-4 md:py-6 ">
+        <div className="container mx-auto flex items-center justify-between gap-x-6">
+          <img src="/public/assets/lws-logo-en.svg" alt="LWS Logo" />
+        </div>
+      </nav>
+    </div>
+  );
+};
+
+export default Header;
